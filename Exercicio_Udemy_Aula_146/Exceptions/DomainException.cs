393b5b5be9576849d54a27711839b550cbdb88ac
André Ramaldes <67028873+Andre07-creator@users.noreply.github.com>
2021-04-21 20:35:43 -0300

    Starting 1.0

---
+++ Exercicio_Udemy_Aula_146/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Udemy_Aula_146.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException (string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Exercicio_Udemy_Aula_146/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Exercicio_Udemy_Aula_146.Entites;
+using Exercicio_Udemy_Aula_146.Exceptions;
+namespace Exercicio_Udemy_Aula_146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number:");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawlimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account account = new Account(number, holder, balance, withdrawlimit);
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(amount);
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
